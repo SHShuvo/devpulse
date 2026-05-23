9314efb199fa603183807d2dddd21c0607760a49
sajjadshuvo <sajjadshuvo1212@gmail.com>
2026-05-23 14:17:09 +0600

    sign up complete

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { type Application, type Request, type Response } from "express";
+import { authRouter } from "./modules/auth/auth.routes";
 
 const app : Application = express();
 app.use(express.json());
@@ -9,5 +10,7 @@ app.use(express.urlencoded({extended : true}));
 app.get("/", (req : Request, res : Response) => {
   res.send("Welcome to DevPulse");
 });
+
+app.use("/api/auth", authRouter);
 
 export default app;
